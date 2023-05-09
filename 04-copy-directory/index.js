1fca132008332d7993ee1df2ff139ceefc3c6175
Vladimir Propp <vovpropp@gmail.com>
2023-05-09 16:49:52 +0200

    feat: add tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,7 +7,16 @@ const destDir = path.join(__dirname,'files-copy');
 copyDir(sourceDir, destDir);
 
 function copyDir(sourceDir, destDir) {
+    
   fs.promises.mkdir(destDir,{recursive:true});
+
+  fs.promises.readdir(destDir)
+    .then(files => {
+      files.forEach(file => {
+        const destFile = path.join(destDir,file);
+        fs.promises.unlink(destFile);
+      });
+    });
   
   fs.promises.readdir(sourceDir)
     .then(files => {
